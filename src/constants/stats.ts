5656b71b8a6ba7012839e04b39d095c52f9b8636
Ruslan Pachov <cao-macao@mail.ru>
2026-08-13 09:57:32 +0300

    fix

--- src/constants/stats.ts
+++ src/constants/stats.ts
@@ -6,7 +6,7 @@ export type Stat = {
 
 export const STATS: Stat[] = [
   { id: 'experience', value: '8', label: 'лет опыта' },
-  { id: 'projects', value: '47+', label: 'выполненных объявлений/проектов' },
+  { id: 'projects', value: '47+', label: 'домов построено' },
   { id: 'rating', value: '900+ ', label: 'кубов бетона залито' },
   { id: 'response', value: '4000м2+', label: 'кровель смонтированно' },
 ];
